e0232b7e253781ca107c05bf86063f3d1148d45e
Sibusiso <sibusisok32@gmail.com>
2019-01-30 04:19:15 +0200

    final changes of lesson 6  video3

--- app/src/androidTest/java/com/jwhh/jim/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/jwhh/jim/notekeeper/NextThroughNotesTest.java
@@ -1,5 +1,8 @@
 package com.jwhh.jim.notekeeper;
 
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v7.widget.RecyclerView;
 
@@ -8,19 +11,18 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static android.support.test.espresso.ViewAction.*;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.*;
-import static android.support.test.espresso.contrib.NavigationViewActions.*;
-import static android.support.test.espresso.contrib.RecyclerViewActions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.AllOf.allOf;
+
 
 public class NextThroughNotesTest {
     @Rule
@@ -44,7 +46,7 @@ public class NextThroughNotesTest {
             onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
 
             if (index < notes.size() - 1)
-                onView(allOf(withId(R.id.action_next), isEnabled()).perform(click()));
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click());
         }
 
         onView(withId(R.id.action_next)).check(matches(not(isEnabled())));
