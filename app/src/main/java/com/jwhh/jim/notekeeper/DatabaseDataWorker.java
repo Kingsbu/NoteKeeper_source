031b7869d9d4ed796fa5001916831a7c0fc9e54e
Sibusiso <sibusisok32@gmail.com>
2019-02-04 08:31:48 +0200

    lesson1 video4

---
+++ app/src/main/java/com/jwhh/jim/notekeeper/DatabaseDataWorker.java
@@ -0,0 +1,50 @@
+package com.jwhh.jim.notekeeper;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatabaseDataWorker {
+    private SQLiteDatabase mDb;
+
+    public DatabaseDataWorker(SQLiteDatabase db){
+        mDb = db;
+    }
+
+    public void insertCourses(){
+        insertCourse("android_intents","Android Programming with Intents");
+        insertCourse("android_async","Android Async Programming and Services");
+        insertCourse("java_lang","Java Fundamentals: The Java Language");
+        insertCourse("java_lang","Java Fundamentals: The Core Platform");
+    }
+
+    public void insertSampleNotes(){
+        insertNote("android_intents", "Dynamic intent resolution", "Wow, intents allow components to be resolved at runtime");
+        insertNote("android_intents", "Delegating intents", "PendingIntents are powerful; they delegate much more than just a component invocation");
+
+        insertNote("android_async", "Service defualt threads", "Did you know that by default an Android Service will tie up the UI thread");
+        insertNote("android_async", "Long running operations", "Foreground Services can be tied to a notifiation icon");
+
+        insertNote("java_lang", "Parameters", "Leverage variable-length parameter lists?");
+        insertNote("java lang", "Anonymous classes", "Anonymous classes simplify implementing one-use types");
+
+        insertNote("java_core", "Compiler options", "The -jar option isn't compatible with the -cp option");
+        insertNote("java_core", "Serialisation", "Remember to include SerialVersionUID to assure version compatibility");
+    }
+
+    private void insertNote(String courseId, String title, String text) {
+        ContentValues values = new ContentValues();
+        values.put(NoteKeeperDatabaseContract.NoteInfoEntry.COLUMN_COURSE_ID, courseId);
+        values.put(NoteKeeperDatabaseContract.NoteInfoEntry.COLUMN_NOTE_TITLE, title);
+        values.put(NoteKeeperDatabaseContract.NoteInfoEntry.COLUMN_NOTE_TEXT, text);
+
+        long newRowId = mDb.insert(NoteKeeperDatabaseContract.NoteInfoEntry.TABLE_NAME, null, values);
+    }
+
+    private void insertCourse(String courseId, String title) {
+        ContentValues values = new ContentValues();
+        values.put(NoteKeeperDatabaseContract.CourseInfoEntry.COLUMN_COURSE_ID, courseId);
+        values.put(NoteKeeperDatabaseContract.CourseInfoEntry.COLUMN_COURSE_TITLE, title);
+
+        long nweRowId = mDb.insert(NoteKeeperDatabaseContract.CourseInfoEntry.TABLE_NAME, null, values);
+    }
+}
